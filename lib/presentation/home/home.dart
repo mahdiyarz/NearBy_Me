e9091393dbd227eeee99b5adc3c021449aeac3ef
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-03-26 06:11:37 +0430

    Create first impression of first page
    Create text form field for searching and set stores list widget after that. Also create a mock data for now on home screen.

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,20 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:nearby_me/helpers/getx_controller.dart';
+import 'package:nearby_me/models/stores_model.dart';
+import 'package:nearby_me/presentation/home/local_widgets/stores_list.dart';
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+class Home extends StatelessWidget {
+  Home({super.key});
 
-  @override
-  State<Home> createState() => _HomeState();
-}
+  MyController getController = Get.put<MyController>(MyController());
 
-class _HomeState extends State<Home> {
+  final TextEditingController _searchController = TextEditingController();
+
+  final List<StoresModel> _mockData = [
+    StoresModel(
+      name: 'Coffee Shop',
+      address: 'Tafazoli blv, Laleh street, No. 53',
+      rate: 8.2,
+      phone: 'phone',
+      reviews: 'reviews',
+    ),
+    StoresModel(
+      name: 'Coffee Net',
+      address: 'Tafazoli blv, Laleh street, No. 65',
+      rate: 7.6,
+      phone: 'phone',
+      reviews: 'reviews',
+    ),
+    StoresModel(
+      name: 'Barber Shop',
+      address: 'Tafazoli blv, Laleh street, No. 78',
+      rate: 6.5,
+      phone: 'phone',
+      reviews: 'reviews',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('NearBy App'),
       ),
-      body: Container(),
+      body: ListView(
+        physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics()),
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 8.0,
+              horizontal: 10,
+            ),
+            child: TextFormField(
+              controller: _searchController,
+              autofocus: true,
+              onChanged: getController.updateQuery,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(),
+                label: Text('Search your location'),
+                hintText: 'Exp: London, Amsterdam, ...',
+                hintStyle: TextStyle(color: Colors.grey),
+              ),
+            ),
+          ),
+          StoresList(stores: _mockData),
+        ],
+      ),
     );
   }
 }
