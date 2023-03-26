83b9bf2e20ef2d2cc900aef269278a49796f4871
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-03-26 04:52:45 +0430

    Create stores_model.dart

---
+++ lib/models/stores_model.dart
@@ -0,0 +1,14 @@
+class StoresModel {
+  final String name;
+  final String address;
+  final int rate;
+  final String phone;
+  final String reviews;
+  StoresModel({
+    required this.name,
+    required this.address,
+    required this.rate,
+    required this.phone,
+    required this.reviews,
+  });
+}
