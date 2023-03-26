e384abadfdb150d26fe3d0dfdd9e89ded60023ae
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-03-26 17:34:18 +0430

    Create app route name class

---
+++ lib/views/resources/router.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+
+import '../screens/details/details_screen.dart';
+import '../screens/home/home.dart';
+
+class AppRouteName {
+  static const String homeScreen = '/home';
+  static const String detailsScreen = '/details';
+
+  static List<GetPage> routes = [
+    GetPage(
+      name: homeScreen,
+      page: () => Home(),
+    ),
+    GetPage(
+      name: detailsScreen,
+      page: () => const DetailsScreen(),
+    ),
+  ];
+}
