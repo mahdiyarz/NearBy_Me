6f52eee893f2f77db5c5b7cc46bd80eb1d7f0e39
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-03-27 18:59:33 +0430

    Add not found illustration on images folder

--- lib/views/resources/assets.dart
+++ lib/views/resources/assets.dart
@@ -2,4 +2,5 @@ class AssetsManagement {
   static const String imagePath = 'assets/images/';
 
   static const String searchAround = '${imagePath}search_around.png';
+  static const String notFoundResult = '${imagePath}not_found.png';
 }
