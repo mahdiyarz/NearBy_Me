93b0c1e68341ccb6cc6af1515f4e483824c00a1d
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-03-27 19:53:10 +0430

    Add api error illustration to images folder

--- lib/views/resources/assets.dart
+++ lib/views/resources/assets.dart
@@ -3,4 +3,5 @@ class AssetsManagement {
 
   static const String searchAround = '${imagePath}search_around.png';
   static const String notFoundResult = '${imagePath}not_found.png';
+  static const String apiError = '${imagePath}api_error.png';
 }
