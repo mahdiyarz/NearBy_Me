7d2c9874d2c6d8600f040eb8476c132014fc3284
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-03-27 13:50:38 +0430

    Change details screen UI

--- lib/views/screens/details/details_screen.dart
+++ lib/views/screens/details/details_screen.dart
@@ -1,24 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../helpers/details_screen/details_controller.dart';
+import '../../../views/extensions/space_xy_extension.dart';
 import '../../../models/stores_model.dart';
+
+import 'local_widgets/app_bar_title.dart';
+import 'local_widgets/reviews_list.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final StoresModel storeDetails = Get.arguments;
+    final String storeName = Get.arguments[StoresFields.name];
+    final String storePhone = Get.arguments[StoresFields.phone];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(storeDetails.name),
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        title: AppBarTitle(
+          storeName: storeName,
+          storePhone: storePhone,
+        ),
       ),
-      body: Column(
-        children: [
-          Text('Phone number is ${storeDetails.phone}'),
-          const Text('Reviews:'),
-          const Text('storeDetails.reviews'),
-        ],
+      body: GetBuilder<DetailsController>(
+        builder: (controller) {
+          if (controller.isLoading) {
+            return SizedBox(
+              height: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const CircularProgressIndicator(),
+                  5.0.spaceY,
+                  const Text('Please Wait...')
+                ],
+              ),
+            );
+          }
+          return ReviewsList(
+            storeReviews: controller.storeReviews,
+          );
+        },
       ),
     );
   }
