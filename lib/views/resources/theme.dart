fab4c2d24be615a24039a33ed8ab448001becc2c
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-03-27 13:02:16 +0430

    Create theme.dart
    Create this file to manage themes

---
+++ lib/views/resources/theme.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme(
+      brightness: Brightness.light,
+      primary: ColorsManagement.primary,
+      onPrimary: ColorsManagement.onPrimary,
+      primaryContainer: ColorsManagement.primaryContainer,
+      onPrimaryContainer: ColorsManagement.onPrimaryContainer,
+      secondary: ColorsManagement.secondary,
+      onSecondary: ColorsManagement.onSecondary,
+      error: ColorsManagement.error,
+      onError: ColorsManagement.onError,
+      background: ColorsManagement.background,
+      onBackground: ColorsManagement.onBackground,
+      surface: ColorsManagement.surface,
+      onSurface: ColorsManagement.onSurface,
+      shadow: ColorsManagement.shadow,
+      outline: ColorsManagement.outline,
+    ),
+
+    //* Elevated button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(color: ColorsManagement.onPrimary),
+        backgroundColor: ColorsManagement.primary,
+        foregroundColor: ColorsManagement.onPrimary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(14),
+        ),
+      ),
+    ),
+  );
+}
